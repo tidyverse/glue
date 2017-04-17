3117b7cf6f490e92c0a2ec2e739dce7f03b5e368
Jim Hester <james.f.hester@gmail.com>
2017-04-17 13:47:30 -0400

    Register native routines

---
+++ src/init.c
@@ -0,0 +1,20 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .Call calls */
+extern SEXP glue_(SEXP, SEXP);
+extern SEXP trim_(SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"glue_", (DL_FUNC) &glue_, 2},
+    {"trim_", (DL_FUNC) &trim_, 1},
+    {NULL, NULL, 0}
+};
+
+void R_init_glue(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
