13a81f7b35aa4311cae59b8be0400ff85374c80a
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-05 06:23:01 +0200

    Revert "enable gctorture"
    This reverts commit 3b46da38b6e0c3e25fd31a6950ac181291c11361.

--- tests/testthat/test-glue.R
+++ tests/testthat/test-glue.R
@@ -1,7 +1,5 @@
 context("glue")
 
-gctorture2(99)
-
 test_that("inputs are concatenated, interpolated variables recycled", {
   expect_identical(as_glue(c("testastring1", "testastring2")), glue("test", "a", "string", "{1:2}"))
 })
