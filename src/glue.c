41a2201d964a018ade40f23af8b90397aad27a3e
Jim Hester <james.f.hester@gmail.com>
2017-07-19 16:01:57 -0400

    clang-format

--- src/glue.c
+++ src/glue.c
@@ -1,6 +1,6 @@
+#include "Rinternals.h"
 #include <stdlib.h>
 #include <string.h>
-#include "Rinternals.h"
 
 SEXP set(SEXP x, int i, SEXP val) {
   size_t len = Rf_length(x);
@@ -49,108 +49,108 @@ SEXP glue_(SEXP x, SEXP f, SEXP open_arg, SEXP close_arg) {
   states prev_state = text;
   for (size_t i = 0; i < str_len; ++i) {
     switch (state) {
-      case text: {
-        if (strncmp(&xx[i], open, open_len) == 0) {
-          /* check for open delim doubled */
-          if (strncmp(&xx[i + open_len], open, open_len) == 0) {
-            i += open_len;
-          } else {
-            state = delim;
-            delim_level = 1;
-            start = i + open_len;
-            break;
-          }
-        }
-        if (strncmp(&xx[i], close, close_len) == 0 &&
-            strncmp(&xx[i + close_len], close, close_len) == 0) {
-          i += close_len;
-        }
-
-        str[j++] = xx[i];
-        break;
-      }
-      case escape: {
-        state = prev_state;
-        break;
-      }
-      case single_quote: {
-        if (xx[i] == '\\') {
-          prev_state = single_quote;
-          state = escape;
-        } else if (xx[i] == '\'') {
-          state = delim;
-        }
-        break;
-      }
-      case double_quote: {
-        if (xx[i] == '\\') {
-          prev_state = double_quote;
-          state = escape;
-        } else if (xx[i] == '\"') {
-          state = delim;
-        }
-        break;
-      }
-      case backtick: {
-        if (xx[i] == '\\') {
-          prev_state = backtick;
-          state = escape;
-        } else if (xx[i] == '`') {
-          state = delim;
-        }
-        break;
-      }
-      case comment: {
-        if (xx[i] == '\n') {
-          state = delim;
-        }
-        break;
-      }
-      case delim: {
-        if (strncmp(&xx[i], open, open_len) == 0) {
-          ++delim_level;
-          i += open_len - 1;
-        } else if (strncmp(&xx[i], close, close_len) == 0) {
-          --delim_level;
-          i += close_len - 1;
+    case text: {
+      if (strncmp(&xx[i], open, open_len) == 0) {
+        /* check for open delim doubled */
+        if (strncmp(&xx[i + open_len], open, open_len) == 0) {
+          i += open_len;
         } else {
-          switch (xx[i]) {
-            case '\'':
-              state = single_quote;
-              break;
-            case '"':
-              state = double_quote;
-              break;
-            case '`':
-              state = backtick;
-              break;
-            case '#':
-              state = comment;
-              break;
-          };
+          state = delim;
+          delim_level = 1;
+          start = i + open_len;
+          break;
         }
-        if (delim_level == 0) {
-          // Result of the current glue statement
-          SEXP expr = PROTECT(Rf_ScalarString(
-              Rf_mkCharLen(&xx[start], (i - close_len) + 1 - start)));
-          SEXP call = PROTECT(Rf_lang2(f, expr));
-          SEXP result = PROTECT(Rf_eval(call, R_GlobalEnv));
-
-          // text in between last glue statement
-          str[j] = '\0';
-          SEXP str_ = PROTECT(Rf_ScalarString(Rf_mkCharLenCE(str, j, CE_UTF8)));
-          REPROTECT(out = set(out, k++, str_), out_idx);
-
-          REPROTECT(out = set(out, k++, result), out_idx);
-
-          // Clear the string buffer
-          memset(str, 0, j);
-          j = 0;
-          UNPROTECT(4);
-          state = text;
-        }
-        break;
       }
+      if (strncmp(&xx[i], close, close_len) == 0 &&
+          strncmp(&xx[i + close_len], close, close_len) == 0) {
+        i += close_len;
+      }
+
+      str[j++] = xx[i];
+      break;
+    }
+    case escape: {
+      state = prev_state;
+      break;
+    }
+    case single_quote: {
+      if (xx[i] == '\\') {
+        prev_state = single_quote;
+        state = escape;
+      } else if (xx[i] == '\'') {
+        state = delim;
+      }
+      break;
+    }
+    case double_quote: {
+      if (xx[i] == '\\') {
+        prev_state = double_quote;
+        state = escape;
+      } else if (xx[i] == '\"') {
+        state = delim;
+      }
+      break;
+    }
+    case backtick: {
+      if (xx[i] == '\\') {
+        prev_state = backtick;
+        state = escape;
+      } else if (xx[i] == '`') {
+        state = delim;
+      }
+      break;
+    }
+    case comment: {
+      if (xx[i] == '\n') {
+        state = delim;
+      }
+      break;
+    }
+    case delim: {
+      if (strncmp(&xx[i], open, open_len) == 0) {
+        ++delim_level;
+        i += open_len - 1;
+      } else if (strncmp(&xx[i], close, close_len) == 0) {
+        --delim_level;
+        i += close_len - 1;
+      } else {
+        switch (xx[i]) {
+        case '\'':
+          state = single_quote;
+          break;
+        case '"':
+          state = double_quote;
+          break;
+        case '`':
+          state = backtick;
+          break;
+        case '#':
+          state = comment;
+          break;
+        };
+      }
+      if (delim_level == 0) {
+        // Result of the current glue statement
+        SEXP expr = PROTECT(Rf_ScalarString(
+            Rf_mkCharLen(&xx[start], (i - close_len) + 1 - start)));
+        SEXP call = PROTECT(Rf_lang2(f, expr));
+        SEXP result = PROTECT(Rf_eval(call, R_GlobalEnv));
+
+        // text in between last glue statement
+        str[j] = '\0';
+        SEXP str_ = PROTECT(Rf_ScalarString(Rf_mkCharLenCE(str, j, CE_UTF8)));
+        REPROTECT(out = set(out, k++, str_), out_idx);
+
+        REPROTECT(out = set(out, k++, result), out_idx);
+
+        // Clear the string buffer
+        memset(str, 0, j);
+        j = 0;
+        UNPROTECT(4);
+        state = text;
+      }
+      break;
+    }
     };
   }
 
