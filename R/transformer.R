0958ddbb93cb8ca0df0098f25e8fe972131be163
Jim Hester <james.f.hester@gmail.com>
2017-11-27 08:42:11 -0500

    Export the identity_transformer

--- R/transformer.R
+++ R/transformer.R
@@ -4,7 +4,8 @@
 #' consistent interface than the default functions.
 #' If `data` is `NULL` then the code is evaluated in the environment. If `data`
 #' is not `NULL` than the code is evaluated in the `data` object first, with
-#' the enclosing environment of `envir`.
+#' the enclosing environment of `envir`. `identity_transformer` is simply a
+#' call to evaluate.
 #'
 #' This function is designed to be used within transformers to evaluate the
 #' code in the glue block.
@@ -15,4 +16,6 @@ evaluate <- function(code, envir) {
   eval(parse(text = code, keep.source = FALSE), envir)
 }
 
+#' @rdname evaluate
+#' @export
 identity_transformer <- evaluate
