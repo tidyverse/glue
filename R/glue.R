43632965fee12cbf971d4e27734326c0b64237c0
Jim Hester <james.f.hester@gmail.com>
2018-05-24 16:57:00 -0400

    Do not count coverage of deprecated functions

--- R/glue.R
+++ R/glue.R
@@ -172,12 +172,14 @@ glue_collapse <- function(x, sep = "", width = Inf, last = "") {
   as_glue(x)
 }
 
+# nocov start
 #' @rdname glue-deprecated
 #' @export
 collapse <- function(x, sep = "", width = Inf, last = "") {
   .Deprecated("glue_collapse", package = "glue")
   glue_collapse(x, sep, width, last)
 }
+# nocov end
 
 #' Trim a character vector
 #'
