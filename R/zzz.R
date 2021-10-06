8ba80fe50fffd0db8c32066c8a60fe162611e0ed
Jim Hester <james.f.hester@gmail.com>
2021-10-06 15:50:33 -0400

    Use rlang compat file for s3_register

--- R/zzz.R
+++ R/zzz.R
@@ -1,56 +1,16 @@
 # nocov start
 .onLoad <- function(...) {
-  register_s3_method("testthat", "compare", "glue")
+  s3_register("testthat::compare", "glue")
 
-  on_package_load("waldo", {
-    register_s3_method("waldo", "compare_proxy", "glue")
-  })
+  s3_register("waldo::compare_proxy", "glue")
 
-  # Register on package load because vctrs depends on glue and will
-  # not be fully loaded when glue is loaded
-  on_package_load("vctrs", {
-    register_s3_method("vctrs", "vec_ptype2", "glue.glue")
-    register_s3_method("vctrs", "vec_ptype2", "character.glue")
-    register_s3_method("vctrs", "vec_ptype2", "glue.character")
-    register_s3_method("vctrs", "vec_cast", "glue.glue")
-    register_s3_method("vctrs", "vec_cast", "character.glue")
-    register_s3_method("vctrs", "vec_cast", "glue.character")
-  })
+  s3_register("vctrs::vec_ptype2", "glue.glue")
+  s3_register("vctrs::vec_ptype2", "character.glue")
+  s3_register("vctrs::vec_ptype2", "glue.character")
+  s3_register("vctrs::vec_cast", "glue.glue")
+  s3_register("vctrs::vec_cast", "character.glue")
+  s3_register("vctrs::vec_cast", "glue.character")
 
   invisible()
 }
-
-register_s3_method <- function(pkg, generic, class, fun = NULL) {
-  stopifnot(is.character(pkg), length(pkg) == 1)
-  stopifnot(is.character(generic), length(generic) == 1)
-  stopifnot(is.character(class), length(class) == 1)
-
-  if (is.null(fun)) {
-    fun <- get(paste0(generic, ".", class), envir = parent.frame())
-  } else {
-    stopifnot(is.function(fun))
-  }
-
-  if (pkg %in% loadedNamespaces()) {
-    registerS3method(generic, class, fun, envir = asNamespace(pkg))
-  }
-
-  # Always register hook in case package is later unloaded & reloaded
-  setHook(
-    packageEvent(pkg, "onLoad"),
-    function(...) {
-      registerS3method(generic, class, fun, envir = asNamespace(pkg))
-    }
-  )
-}
-
-on_package_load <- function(pkg, expr) {
-  if (isNamespaceLoaded(pkg)) {
-    expr
-  } else {
-    thunk <- function(...) expr
-    setHook(packageEvent(pkg, "onLoad"), thunk)
-  }
-}
-
 #nocov end
