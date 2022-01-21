01fd09db3b5bd48fb1707b960a908f34a240f751
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-01-20 22:24:30 -0800

    Remove dummy comment

--- R/zzz.R
+++ R/zzz.R
@@ -11,7 +11,6 @@
   s3_register("vctrs::vec_cast", "character.glue")
   s3_register("vctrs::vec_cast", "glue.character")
 
-  # comment
   if (isNamespaceLoaded("knitr") &&
       "knit_engines" %in% getNamespaceExports("knitr")) {
     knitr::knit_engines$set(glue = eng_glue, glue_sql = eng_glue_sql, gluesql = eng_glue_sql)
