ba7768d0726ec78cd39b351cea0fdfb1b9299fdf
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-01-20 21:55:37 -0800

    Ensure knitr::knit_engines is accessible

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,8 @@
   s3_register("vctrs::vec_cast", "glue.character")
 
   # comment
-  if (isNamespaceLoaded("knitr")) {
+  if (isNamespaceLoaded("knitr") &&
+      "knit_engines" %in% getNamespaceExports("knitr")) {
     knitr::knit_engines$set(glue = eng_glue, glue_sql = eng_glue_sql, gluesql = eng_glue_sql)
   } else {
     setHook(packageEvent("knitr", "onLoad"), function(...) {
