5a1650292ae1effd38c123d149ab0c9632252965
Lionel Henry <lionel.hry@gmail.com>
2022-11-18 17:42:35 +0100

    Support hidden visibility for compatibility with webR (#284)
    * Support hidden visibility
    
    * Add NEWS bullet
    
    Co-authored-by: Jenny Bryan <jenny.f.bryan@gmail.com>

--- src/init.c
+++ src/init.c
@@ -3,6 +3,11 @@
 #include <Rinternals.h>
 #include <stdlib.h> // for NULL
 
+// Compile with `C_VISIBILITY = -fvisibility=hidden` if you link to
+// this library
+#include <R_ext/Visibility.h>
+#define export attribute_visible extern
+
 /* .Call calls */
 extern SEXP glue_(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP trim_(SEXP);
@@ -12,6 +17,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"trim_", (DL_FUNC)&trim_, 1},
     {NULL, NULL, 0}};
 
+export
 void R_init_glue(DllInfo* dll) {
   R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
