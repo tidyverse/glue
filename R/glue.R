963ea6a43d21c2224b2d5a6747e409abf48e4443
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-01-09 16:46:31 -0800

    Actually check that `.envir` is an environment
    The return of the fix for #308

--- R/glue.R
+++ R/glue.R
@@ -97,6 +97,7 @@ glue_data <- function(.x, ..., .sep = "", .envir = parent.frame(),
 
   # Perform all evaluations in a temporary environment
   if (is.null(.x)) {
+    stopifnot(is.environment(.envir))
     parent_env <- .envir
   } else if (is.environment(.x)) {
     parent_env <- .x
