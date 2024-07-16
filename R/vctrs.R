5e9c87829542e47236467154a3416744f7f2459e
Hadley Wickham <h.wickham@gmail.com>
2024-07-16 13:58:39 -0500

    Re-document with latest roxygen2 features (#310)

--- R/vctrs.R
+++ R/vctrs.R
@@ -1,23 +1,26 @@
-
-# Registered in .onLoad()
-
+#' @exportS3Method vctrs::vec_ptype2
 vec_ptype2.glue.glue <- function(x, y, ...) {
   x
 }
+#' @exportS3Method vctrs::vec_ptype2
 vec_ptype2.glue.character <- function(x, y, ...) {
   x
 }
+#' @exportS3Method vctrs::vec_ptype2
 vec_ptype2.character.glue <- function(x, y, ...) {
   y
 }
 
 # Note order of class is the opposite as for ptype2
+#' @exportS3Method vctrs::vec_cast
 vec_cast.glue.glue <- function(x, to, ...) {
   x
 }
+#' @exportS3Method vctrs::vec_cast
 vec_cast.glue.character <- function(x, to, ...) {
   as_glue(x)
 }
+#' @exportS3Method vctrs::vec_cast
 vec_cast.character.glue <- function(x, to, ...) {
   unclass(x)
 }
