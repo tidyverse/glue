ed413d25031b926832003ab80e042d6f32922e26
Davis Vaughan <davis@posit.co>
2025-08-12 14:37:05 -0400

    Revert back to `s3_register()` (#347)
    * Revert back to `s3_register()`
    
    * Replace TODO with PR number

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,14 @@
 # nocov start
 .onLoad <- function(...) {
+  # Do not swap these for `@exportS3Method`, which has a nasty bug (#347)
+  s3_register("testthat::compare", "glue")
+  s3_register("waldo::compare_proxy", "glue")
+  s3_register("vctrs::vec_ptype2", "glue.glue")
+  s3_register("vctrs::vec_ptype2", "character.glue")
+  s3_register("vctrs::vec_ptype2", "glue.character")
+  s3_register("vctrs::vec_cast", "glue.glue")
+  s3_register("vctrs::vec_cast", "character.glue")
+  s3_register("vctrs::vec_cast", "glue.character")
 
   if (isNamespaceLoaded("knitr") &&
       "knit_engines" %in% getNamespaceExports("knitr")) {
